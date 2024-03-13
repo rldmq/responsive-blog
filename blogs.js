808a06df9b3b88a9c352426dd79c38b64898a387
rldmq <76823660+rldmq@users.noreply.github.com>
2024-03-13 17:46:11 -0400

    finished basic functionality
    to do:
    -blog page: javascript to remove active blog
    -style for active page in nav (home and about)
    -write notes about the blog in the about me and blog pages
    -fix title for "current" blog to match actual text (see above for context)
    -make logo clickable to navigate to homepage

--- blogs.js
+++ blogs.js
@@ -6,3 +6,12 @@ const blogsContainer = document.getElementById("blogs-container")
 const recentBlogs = blogData.slice(0,3)
 
 renderBlogsGrid(blogsContainer,recentBlogs, true)
+
+document.getElementById("header").innerHTML = `
+    <i class="logo fa-solid fa-book-journal-whills"></i>
+    <h3 class="logo-title">Responsive Blog</h3>
+    <nav class="nav">
+        <a href="../home.html">Home</a>
+        <a href="../about-me.html">About Me</a>
+    </nav>
+`
